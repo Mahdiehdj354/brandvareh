c23e759713840d30dd3f2e0d13cb7c358cab2079
unknown <mahdieh.d.354012@gmail.com>
2022-07-30 09:16:46 +0200

    multiple swiper products

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,46 @@
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 4,
+var swiperSuggestion = new Swiper(".mySwiperSuggestion", {
+  slidesPerView: 1,
   spaceBetween: 10,
   slidesPerGroup: 1,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    960: {
+      slidesPerView: 4,
+      spaceBetween: 10,
+    },
+  },
   loop: true,
   loopFillGroupWithBlank: true,
   pagination: {
-    el: ".swiper-pagination",
+    el: ".swiper-pagination-suggestion",
     clickable: true,
   },
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".swiper-button-next-suggestion",
+    prevEl: ".swiper-button-prev-suggestion",
+  },
+});
+var swiperNewProduct = new Swiper(".mySwiperNewProduct", {
+  slidesPerView: 2,
+  spaceBetween: 10,
+  slidesPerGroup: 1,
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+    960: {
+      slidesPerView: 4,
+      spaceBetween: 10,
+    },
+  },
+  loop: true,
+  loopFillGroupWithBlank: true,
+  navigation: {
+    nextEl: ".swiper-button-next-newProduct",
+    prevEl: ".swiper-button-prev-newProduct",
   },
 });
